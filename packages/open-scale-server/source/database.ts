e0a82d9e38e10869f1fd2493bd5339ba6d186ae0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-11-22 12:26:55 +0200

    open-scale-server > dev: database

--- packages/open-scale-server/source/database.ts
+++ packages/open-scale-server/source/database.ts
@@ -14,21 +14,36 @@ import {
 
 
 
-const OPEN_SCALE_DIR = path.join(
-    homedir(),
-    '/.open-scale',
-);
-await fs.mkdir(OPEN_SCALE_DIR, { recursive: true });
+const setupDatabase = async () => {
+    const OPEN_SCALE_DIR = path.join(
+        homedir(),
+        '/.open-scale',
+    );
+    await fs.mkdir(OPEN_SCALE_DIR, { recursive: true });
 
-const DATABASE_PATH = path.join(
-    OPEN_SCALE_DIR,
-    '/db.json',
-);
-const defaultData: Database = {
-    targetWeight: DEFAULT_TARGET_WEIGHT,
-    errorPercentage: DEFAULT_ERROR_PERCENTAGE,
-    events: [],
-};
+    const DATABASE_PATH = path.join(
+        OPEN_SCALE_DIR,
+        '/db.json',
+    );
+    const defaultData: Database = {
+        targetWeight: DEFAULT_TARGET_WEIGHT,
+        errorPercentage: DEFAULT_ERROR_PERCENTAGE,
+        events: [],
+    };
+
+    try {
+        await fs.access(DATABASE_PATH);
+    } catch (_error) {
+        await fs.writeFile(DATABASE_PATH, JSON.stringify(defaultData));
+    }
+
+    return [
+        DATABASE_PATH,
+        defaultData,
+    ] as const;
+}
+
+const [DATABASE_PATH, defaultData] = await setupDatabase();
 const database = new Low<Database>(new JSONFile(DATABASE_PATH), defaultData);
 
 
