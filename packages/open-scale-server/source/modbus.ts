8c8cc741e938d4a79a1e6684922d6be6492534bd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-12-01 18:07:36 +0200

    open-scale-server > dev: modbus

--- packages/open-scale-server/source/modbus.ts
+++ packages/open-scale-server/source/modbus.ts
@@ -3,10 +3,13 @@ import ModbusRTU from 'npm:modbus-serial';
 
 
 const client = new ModbusRTU.default();
+
 // client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600 }, write);
-client.connectTCP('127.0.0.1', { port: 8502 });
+
+const MODBUS_IP = Deno.env.get('MODBUS_IP') || '127.0.0.1';
+const MODBUS_PORT = parseInt(Deno.env.get('MODBUS_PORT') || '') || 8502;
+client.connectTCP(MODBUS_IP, { port: MODBUS_PORT });
 client.setID(1);
-// client.writeRegisters(5, [0 , 0xffff]);
 
 
 export default client;
