ec9b81d282e3a7fbc7627e8df058ad65b9e8b7bd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-12-03 20:34:32 +0200

    open-scale-server > dev: modbus

--- packages/open-scale-server/source/modbus.ts
+++ packages/open-scale-server/source/modbus.ts
@@ -3,23 +3,25 @@ import ModbusRTU from 'modbus-serial';
 
 
 const client = new ModbusRTU();
-
 const MODBUS_ID = parseInt(process.env.MODBUS_ID || '') || 1;
 
-const MODBUS_RTU = process.env.MODBUS_RTU || '/dev/ttyUSB0';
-const MODBUS_BAUD = parseInt(process.env.MODBUS_BAUD || '') || 9600;
-const MODBUS_PARITY = (process.env.MODBUS_PARITY || 'none') as 'none' | 'even' | 'odd';
-const MODBUS_STOP = parseInt(process.env.MODBUS_STOP || '') || 1;
-client.connectRTUBuffered(MODBUS_RTU, {
-    baudRate: MODBUS_BAUD,
-    parity: MODBUS_PARITY,
-    stopBits: MODBUS_STOP,
-});
-
-// const MODBUS_IP = process.env.MODBUS_IP || '127.0.0.1';
-// const MODBUS_PORT = parseInt(process.env.MODBUS_PORT || '') || 8502;
-// client.connectTCP(MODBUS_IP, { port: MODBUS_PORT });
+if (process.env.MODBUS_RTU === 'true') {
+    const MODBUS_RTU = process.env.MODBUS_RTU || '/dev/ttyUSB0';
+    const MODBUS_BAUD = parseInt(process.env.MODBUS_BAUD || '') || 9600;
+    const MODBUS_PARITY = (process.env.MODBUS_PARITY || 'none') as 'none' | 'even' | 'odd';
+    const MODBUS_STOP = parseInt(process.env.MODBUS_STOP || '') || 1;
+    client.connectRTUBuffered(MODBUS_RTU, {
+        baudRate: MODBUS_BAUD,
+        parity: MODBUS_PARITY,
+        stopBits: MODBUS_STOP,
+    });
+} else {
+    const MODBUS_IP = process.env.MODBUS_IP || '127.0.0.1';
+    const MODBUS_PORT = parseInt(process.env.MODBUS_PORT || '') || 8502;
+    client.connectTCP(MODBUS_IP, { port: MODBUS_PORT });
+}
 
 client.setID(MODBUS_ID);
+
 
 export default client;
