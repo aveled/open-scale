cc7f042deed96db3d95a3dfc208ea79713b8969d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2025-03-27 13:50:49 +0200

    open-scale-server > dev: tester

---
+++ packages/open-scale-server/tester/tester-sensor.ts
@@ -0,0 +1,46 @@
+import gpio from 'rpi-gpio';
+
+
+
+const SENSOR_GPIO = parseInt(process.env.SENSOR_GPIO || '') || 12;
+
+class Sensor {
+    private toggled = false;
+
+    constructor() {
+        this.setup();
+    }
+
+    private setup() {
+        gpio.setup(SENSOR_GPIO, gpio.DIR_IN, gpio.EDGE_BOTH, (error, value) => {
+            if (error || value === undefined) {
+                return;
+            }
+
+            this.toggled = value;
+        });
+    }
+
+    public readState() {
+        return this.toggled;
+    }
+
+    public onUpdate(
+        updater: (value: boolean) => void,
+    ) {
+        gpio.on('change', (channel, value) =>  {
+            if (channel !== SENSOR_GPIO) {
+                return;
+            }
+
+            this.toggled = value === 1;
+            updater(value === 1);
+        });
+    }
+}
+
+
+const sensor = new Sensor();
+sensor.onUpdate((value) => {
+    console.log(value);
+});
